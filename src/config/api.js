200dda2a1cdd2a254b5937af3ae908fc2989f777
VamsiReddy1231 <142213271+VamsiReddy1231@users.noreply.github.com>
2024-12-08 04:44:27 +0530

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -1,9 +1,10 @@
 
 import axios from 'axios';
 // const DEPLOYED='https://e-commerce-server-production-0873.up.railway.app'
-const LOCALHOST='http://localhost:2024'
+// const LOCALHOST='http://localhost:2024'
 
-export const API_BASE_URL = LOCALHOST
+export const API_BASE_URL =  "https://tribalprojectbackend-production.up.railway.app"
+
 
 const api = axios.create({
   baseURL: API_BASE_URL,
